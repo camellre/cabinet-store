ba2e6b6b58602d7e88c707d653ffd4fb90021e1f
Rory <camellre@gmail.com>
2022-11-23 00:11:33 -0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+        atque magni numquam, natus perspiciatis alias aut, hic, odio porro eaque
+        similique est aliquam doloribus blanditiis modi quaerat dolore dolores
+        eius!
+      </p>
     </div>
   );
 }
