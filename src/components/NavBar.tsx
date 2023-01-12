f86bfc73dcac0e29a19b8893a98a4411624c7cef
Rory <camellre@gmail.com>
2023-01-12 08:30:41 -0800

    added NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import style from "../css/NavBar.module.scss";
+import iconStyle from "../css/icon.module.scss";
+import { ReactComponent as Toggler } from "../images/search.svg";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      <nav className={style.nav}>
+        <a href="/">
+          <img src="../images/home.svg" alt="" />
+        </a>
+        <Toggler className={iconStyle.icon} />
+      </nav>
+    </div>
+  );
 }
