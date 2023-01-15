dafc0d597fa4c61b9f9f00efd7959ebd1152a182
Rory <camellre@gmail.com>
2023-01-14 20:58:53 -0800

    built navbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,7 @@ import HomePageGrid from "./components/HomePageGrid";
 import NavBar from "./components/NavBar";
 import PromotionBar from "./components/PromotionBar";
 
-interface ListItem {
-  id: string;
-  text: string;
-}
-
 function App() {
-  const listItem: ListItem[] = [
-    {
-      id: "1",
-      text: "Item 12",
-    },
-    {
-      id: "2",
-      text: "Item 2",
-    },
-    {
-      id: "3",
-      text: "Item 3",
-    },
-  ];
-
-  let selectedListItem: string | null = "Item 3";
-  const handleItemSelect = () => {
-    console.log("itemselected");
-  };
-
   return (
     <>
       <NavBar />
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,40 @@
 import React from "react";
 import style from "../css/NavBar.module.scss";
-import iconStyle from "../css/icon.module.scss";
+import iconStyle from "../css/Icon.module.scss";
+import listStyle from "../css/List.module.scss";
 import { ReactComponent as Toggler } from "../images/search.svg";
+import { ReactComponent as HomeIcon } from "../images/home.svg";
 
 export default function NavBar() {
   return (
     <div>
       <nav className={style.nav}>
         <a href="/">
-          <img src="../images/home.svg" alt="" />
+          <HomeIcon className={iconStyle.icon} />
         </a>
         <Toggler className={iconStyle.icon} />
+        <ul className={listStyle.list + " " + listStyle.navList}>
+          <li className={style.navItem}>
+            <a className={style.navItemLink} href="#">
+              Shop
+            </a>
+          </li>
+          <li className={style.navItem}>
+            <a className={style.navItemLink} href="#">
+              Design
+            </a>
+          </li>
+          <li className={style.navItem}>
+            <a className={style.navItemLink} href="#">
+              About Us
+            </a>
+          </li>
+          <li className={style.navItem}>
+            <a className={style.navItemLink} href="#">
+              Help
+            </a>
+          </li>
+        </ul>
       </nav>
     </div>
   );
