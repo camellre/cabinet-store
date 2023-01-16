da78dcdba1cdf889414b4b67a69d1b151e47e0eb
Rory <camellre@gmail.com>
2023-01-15 21:32:42 -0800

    updated FeatureBar

--- src/components/FeatureBar.tsx
+++ src/components/FeatureBar.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import style from "../css/FeatureBar.module.scss";
 
 export default function FeatureBar() {
-  return <div>FeatureBar</div>;
+  return (
+    <div className={style.gridContainer}>
+      <p className={style.gridItem}>Feature 1</p>
+      <p className={style.gridItem}>Feature 2</p>
+      <p className={style.gridItem}>Feature 3</p>
+      <p className={style.gridItem}>Feature 4</p>
+    </div>
+  );
 }
