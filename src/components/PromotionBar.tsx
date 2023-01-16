701a1b8f576599f5f7040aa9ae899cafb674166f
Rory <camellre@gmail.com>
2023-01-15 21:17:38 -0800

    update PromotionBar

--- src/components/PromotionBar.tsx
+++ src/components/PromotionBar.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import style from "../css/PromotionBar.module.scss";
+import promotion_1 from "../images/promotion_1.jpg";
+import promotion_2 from "../images/promotion_2.jpg";
 
 export default function PromotionBar() {
-  return <div className={style.gridContainer}></div>;
+  return (
+    <div className={style.gridContainer}>
+      <img className={style.gridItem_1} src={promotion_1} alt="" />
+      <img className={style.gridItem_2} src={promotion_2} alt="" />
+    </div>
+  );
 }
