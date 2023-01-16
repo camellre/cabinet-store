c975f9928ebf12e7bd3264ca95e5d522990fdbaa
Rory <camellre@gmail.com>
2023-01-15 20:34:03 -0800

    added promotionBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.scss";
-import Button from "./commoncomponents/Button";
-import Icon from "./commoncomponents/Icon";
 import FeatureBar from "./components/FeatureBar";
 import HomePageFooter from "./components/HomePageFooter";
 import HomePageGrid from "./components/HomePageGrid";
 import NavBar from "./components/NavBar";
 import PromotionBar from "./components/PromotionBar";
-import { ReactComponent as Home } from "./images/home.svg";
 
 function App() {
   return (
@@ -17,8 +14,6 @@ function App() {
       <FeatureBar />
       <HomePageGrid />
       <HomePageFooter />
-      <Button theme={"navBarButton"} text={"Sign Up"} />
-      <Icon IconImage={Home} />
     </>
   );
 }
--- src/components/PromotionBar.tsx
+++ src/components/PromotionBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import style from "../css/PromotionBar.module.scss";
 
 export default function PromotionBar() {
-  return <div>PromotionBar</div>;
+  return <div className={style.gridContainer}></div>;
 }
