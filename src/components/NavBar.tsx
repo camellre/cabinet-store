920b0466cc8f75912981f59908d665031e0146ed
Rory <camellre@gmail.com>
2023-01-31 22:45:08 -0800

    added NavBar Toggler

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,26 +4,30 @@ import Icon from "../commoncomponents/Icon";
 import { ReactComponent as NavBarImageHome } from "../images/home.svg";
 import { ReactComponent as NavBarImageSearch } from "../images/search.svg";
 import { ReactComponent as NavBarImageUser } from "../images/user.svg";
+import { ReactComponent as NavBarImageSettings } from "../images/settings.svg";
 
 export default function NavBar() {
   return (
     <div className={style.navBarContainer}>
+      <span className={style.navBarToggler}>
+        <Icon IconImage={NavBarImageSettings} />
+      </span>
       <Icon IconImage={NavBarImageHome} />
       <div className={style.navBarMenuContainer}>
-        <ul className={style.navBarMenu}>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#shop">Shop</a>
-          </li>
-          <li>
-            <a href="#about">About Us</a>
-          </li>
-          <li>
-            <a href="#contact">Contact Us</a>
-          </li>
-        </ul>
+        <div className={style.navBarMenu}>
+          <a className={style.navBarMenuItem} href="#home">
+            Home
+          </a>
+          <a className={style.navBarMenuItem} href="#shop">
+            Shop
+          </a>
+          <a className={style.navBarMenuItem} href="#about">
+            About Us
+          </a>
+          <a className={style.navBarMenuItem} href="#contact">
+            Contact Us
+          </a>
+        </div>
         <Icon IconImage={NavBarImageSearch} />
         <Icon IconImage={NavBarImageUser} />
       </div>
