722b136b7b2e48f2a4430ea2a1784b2673d8b9bb
Rory <camellre@gmail.com>
2023-02-24 23:10:57 -0800

    Updated NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,48 +6,8 @@ import { ReactComponent as NavBarImageSearch } from "../images/search.svg";
 import { ReactComponent as NavBarImageUser } from "../images/user.svg";
 import { ReactComponent as NavBarImageSettings } from "../images/settings.svg";
 
-interface DropdownMenu {
-  [key: string]: JSX.Element;
-}
-
 export default function NavBar() {
   const iconContainer = <Icon IconImage={NavBarImageSettings} />;
-  const [dropdownMenuContent, setDropdownMenuContent] = useState(<></>);
-
-  const dropdownMenu: DropdownMenu = {
-    home: (
-      <div className={style.navBarDropdownMenu}>
-        <a href="#1">Home1</a>
-        <a href="#2">Home2</a>
-        <a href="#3">Home3</a>
-        <a href="#4">Home4</a>
-      </div>
-    ),
-    shop: (
-      <div className={style.navBarDropdownMenu}>
-        <a href="#1">Shop1</a>
-        <a href="#2">Shop2</a>
-        <a href="#3">Shop3</a>
-        <a href="#4">Shop4</a>
-      </div>
-    ),
-    about: (
-      <div className={style.navBarDropdownMenu}>
-        <a href="#1">About1</a>
-        <a href="#2">About2</a>
-        <a href="#3">About3</a>
-        <a href="#4">About4</a>
-      </div>
-    ),
-    contact: (
-      <div className={style.navBarDropdownMenu}>
-        <a href="#1">Contact1</a>
-        <a href="#2">Contact2</a>
-        <a href="#3">Contact3</a>
-        <a href="#4">Contact4</a>
-      </div>
-    ),
-  };
 
   const [navBarStyle, setNavBarStyle] = useState(style.navBarBackground);
 
@@ -58,73 +18,49 @@ export default function NavBar() {
     setNavBarStyle(style.navBarBackground);
   };
 
-  const handleDropdown = (
-    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
-  ) => {
-    const menuName = e.currentTarget.id;
-    console.log(menuName);
-    setDropdownMenuContent(dropdownMenu[menuName]);
-    console.log(dropdownMenu[menuName]);
-  };
-
-  const handleDropdownLeave = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    setDropdownMenuContent(<></>);
-  };
-
   return (
-    <div
-      className={navBarStyle}
-      onMouseLeave={(e) => {
-        handleDropdownLeave(e);
-      }}
-    >
+    <div className={navBarStyle}>
       <div className={style.navBarContainer}>
         <span onClick={handleTogglerClick} className={style.navBarToggler}>
           {iconContainer}
         </span>
         <Icon IconImage={NavBarImageHome} />
         <div className={style.navBarMenuContainer}>
-          <a
-            id="home"
-            onMouseEnter={(e) => handleDropdown(e)}
-            className={style.navBarMenuItem}
-            href="#home"
-          >
-            Home
-          </a>
-          <a
-            id="shop"
-            onMouseEnter={(e) => handleDropdown(e)}
-            className={style.navBarMenuItem}
-            href="#shop"
-          >
+          <div className={style.navBarMenuItemContainer}>
+            <a className={style.navBarMenuItem} href="#home">
+              Home
+            </a>
+            <div className={style.navBarDropdownMenuContainer}>
+              <a className={style.navBarDropdownMenuItem} href="#link1">
+                Link 1
+              </a>
+              <a className={style.navBarDropdownMenuItem} href="#link2">
+                Link 2
+              </a>
+              <a className={style.navBarDropdownMenuItem} href="#link3">
+                Link 3
+              </a>
+              <a className={style.navBarDropdownMenuItem} href="#link4">
+                Link 4
+              </a>
+            </div>
+          </div>
+          <a id="shop" className={style.navBarMenuItem} href="#shop">
             Shop
           </a>
-          <a
-            id="about"
-            onMouseEnter={(e) => handleDropdown(e)}
-            className={style.navBarMenuItem}
-            href="#about"
-          >
+          <a id="about" className={style.navBarMenuItem} href="#about">
             About Us
           </a>
-          <a
-            id="contact"
-            onMouseEnter={(e) => handleDropdown(e)}
-            className={style.navBarMenuItem}
-            href="#contact"
-          >
+          <a id="contact" className={style.navBarMenuItem} href="#contact">
             Contact Us
           </a>
         </div>
-        <div className={style.navBarMenuTools}>
+        <div className={style.navBarMenuToolContainer}>
           <Icon IconImage={NavBarImageSearch} />
           <Icon IconImage={NavBarImageUser} />
         </div>
       </div>
-      <div className={style.navBarMobileMenu}>
+      <div className={style.navBarMobileMenuContainer}>
         <a className={style.navBarMenuItem} href="#home">
           Home
         </a>
@@ -138,7 +74,6 @@ export default function NavBar() {
           Contact Us
         </a>
       </div>
-      {dropdownMenuContent}
     </div>
   );
 }
